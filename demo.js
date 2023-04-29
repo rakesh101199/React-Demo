b1ccea126f6e2f409fc29449969bfc754b2f6461
rakesh101199 <rakesh101199@gmail.com>
2023-04-29 16:27:56 +0530

    Second changes

--- demo.js
+++ demo.js
@@ -1,1 +1,7 @@
-console.log("Hello to Demo GitWorld");
+var listItems= document.getElementsByClassName('list-group-item');
+//console.log(listItems[2]);
+listItems[2].style.backgroundColor='green';
+
+for(let i=0;i<listItems.length;i++){
+    listItems[i].style.fontWeight='bold';
+}
