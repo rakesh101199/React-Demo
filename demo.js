531c3daefa177d90c0ba3fae02990fb0cfa1c857
rakesh101199 <rakesh101199@gmail.com>
2023-04-29 17:38:44 +0530

    fifth

--- demo.js
+++ demo.js
@@ -1,22 +1,18 @@
-var listItems= document.getElementsByClassName('list-group-item');
-//console.log(listItems[2]);
-listItems[1].style.backgroundColor='black';
-listItems[3].style.display='None';
-
-var item=document.querySelector('.list-group-item:nth-child(2)');
-item.style.color='green';
-console.log(item);
-
-var items=document.querySelectorAll('.list-group-item:nth-child(odd)')
+var newText=document.createElement('div');
+var text=document.createTextNode('Hello');
+newText.appendChild(text);
+var items=document.querySelector('header .container');
+var item=document.querySelector('header h1');
 console.log(items);
+console.log(item);
+items.insertBefore(newText,item);
 
-for(let i=0;i<items.length;i++){
-    items[i].style.backgroundColor='brown';
-}
-
-
-
-
-
-
-
+var newText1=document.createElement('div');
+var text1=document.createTextNode('Hello');
+newText1.appendChild(text1);
+newText1.className='list-group-item';
+var items1=document.querySelector('#items');
+var item1=document.querySelector(".list-group-item");
+console.log(items1);
+console.log(item1);
+items1.insertBefore(newText1,item1);
