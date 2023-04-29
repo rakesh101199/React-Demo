043625580da3754e083495aa0cbbd24e9e19a815
rakesh101199 <rakesh101199@gmail.com>
2023-04-29 16:53:42 +0530

    fourthcommit

--- demo.js
+++ demo.js
@@ -1,19 +1,20 @@
-/*var listItems= document.getElementsByClassName('list-group-item');
+var listItems= document.getElementsByClassName('list-group-item');
 //console.log(listItems[2]);
-listItems[2].style.backgroundColor='green';
+listItems[1].style.backgroundColor='black';
+listItems[3].style.display='None';
 
-for(let i=0;i<listItems.length;i++){
-    listItems[i].style.fontWeight='bold';
+var item=document.querySelector('.list-group-item:nth-child(2)');
+item.style.color='green';
+console.log(item);
+
+var items=document.querySelectorAll('.list-group-item:nth-child(odd)')
+console.log(items);
+
+for(let i=0;i<items.length;i++){
+    items[i].style.backgroundColor='brown';
 }
 
-*/
-var listItem= document.getElementsByTagName('li');
-//console.log(listItems[2]);
-listItem[2].style.backgroundColor='green';
 
-for(let i=0;i<listItems.length;i++){
-    listItem[i].style.fontWeight='bold';
-}
 
 
 
