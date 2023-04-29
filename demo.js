b6f1f81f1027dfc4f5d6e2feb3a0b7ab4ec04844
rakesh101199 <rakesh101199@gmail.com>
2023-04-29 16:37:12 +0530

    Thirdcommit

--- demo.js
+++ demo.js
@@ -1,7 +1,21 @@
-var listItems= document.getElementsByClassName('list-group-item');
+/*var listItems= document.getElementsByClassName('list-group-item');
 //console.log(listItems[2]);
 listItems[2].style.backgroundColor='green';
 
 for(let i=0;i<listItems.length;i++){
     listItems[i].style.fontWeight='bold';
 }
+
+*/
+var listItem= document.getElementsByTagName('li');
+//console.log(listItems[2]);
+listItem[2].style.backgroundColor='green';
+
+for(let i=0;i<listItems.length;i++){
+    listItem[i].style.fontWeight='bold';
+}
+
+
+
+
+
