89a1c349f9059485176be8265b843b62aa289cc0
rakesh101199 <rakesh101199@gmail.com>
2023-05-01 17:10:30 +0530

    addedDelete

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 var form = document.getElementById('my-form');
+var users = document.getElementById('users');
+users.addEventListener('click',delFunction);
 
 form.addEventListener('submit',onSubmit);
+//deleting the user
+function delFunction(e){
+    e.preventDefault();
+    if(e.target.id === 'dele'){
+        var par=e.target.parentElement;
+        users.removeChild(par);
+    }
+
+}
+
+
+
+
+
 // storing all the users data
 const myObj ={};
 function onSubmit(e){
@@ -13,8 +29,21 @@ function onSubmit(e){
     localStorage.setItem('myobj',myobj_se);
     var myObj_de=JSON.parse(localStorage.getItem('myobj'));
     console.log(myObj_de);
+     
+    var userBtn = document.createElement('li');
+    var deleteBtn = document.createElement('button');
+    deleteBtn.appendChild(document.createTextNode('delete'));
+    deleteBtn.style.background='red';
+    deleteBtn.style.marginLeft='100px';
+    deleteBtn.id='dele';
+
+
+    var text = document.createTextNode(''+name.value +'  '+ email.value);
+    userBtn.appendChild(text);
+    userBtn.appendChild(deleteBtn);
+    users.appendChild(userBtn);
+
+
     name.value='';
     email.value='';
-
-
 }
