d1cfa1acd5b59b7c8bbac384df16f919705fd77c
rakesh101199 <rakesh101199@gmail.com>
2023-06-20 15:56:58 +0530

    Adding_css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,18 @@
-function ExpenseItem(){
-   return (
-    <div>
-    <h2>Expense Items!</h2>
-    <h3>Food Rs 10</h3>
-    <h3>Petrol Rs 100</h3>
-    <h3>Movies Rs 2000</h3>
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+    const expenseDate = new Date(2023, 7,28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 230;
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}rs</div>
+      </div>
     </div>
-    );
+  );
 }
 
-
-export default ExpenseItem
+export default ExpenseItem;
