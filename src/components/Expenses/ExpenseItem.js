8fcee01a2f2975eda7f88a011627f891424b3c48
rakesh101199 <rakesh101199@gmail.com>
2023-06-23 17:11:07 +0530

    Adding Event Listners

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,33 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
 
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+
   const clickHandler = () => {
-    console.log('deleted');
+    setTitle('updated');
+    console.log(' title clicked');
+  }
+
+  const amountHandler = () => {
+    setAmount(100);
+    console.log('expense clicked');
+
   }
  
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}rs</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}rs</div>
       </div>
-      <button onClick={clickHandler}>Delete Expense</button>
+      <button onClick={clickHandler}>Change title</button>
+      <button onClick={amountHandler}>Change Expense</button>
     </Card>
   );
 }
