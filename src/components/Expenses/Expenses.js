c5b1dbd7dc1edebdca0402d4e6f8f4fec184be7a
rakesh101199 <rakesh101199@gmail.com>
2023-06-25 16:54:48 +0530

    addingFilter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,23 @@
+import React, {useState} from "react";
+
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
-function Expenses(props) {
+const Expenses = (props) => {
+
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
+
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       <ExpenseItem
         title={props.items[0].title}
         date={props.items[0].date}
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -50,7 +50,7 @@ function ExpenseForm(props){
         </div>
         <div className='new-expense__control'>
             <label>Date</label>
-            <input type='date' min="2019-01-01" max="2023-12-31" value={EnteredDate} onChange={dateChangeHandler}/>
+            <input type='date' min="2019-01-01" max="2022-12-31" value={EnteredDate} onChange={dateChangeHandler}/>
         </div>
         </div>
         <div className='new-expense__actions'>
